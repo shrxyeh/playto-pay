ea4c5ff4d9ca2fdd35c0f985665e851e112006c8
shrxyeh <shrxyeh@gmail.com>
2026-04-27 16:49:01 +0530

    fix db isolation level config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -56,9 +56,6 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD", default="postgres"),
         "HOST": config("DB_HOST", default="localhost"),
         "PORT": config("DB_PORT", default="5432"),
-        "OPTIONS": {
-            "isolation_level": "read committed",
-        },
     }
 }
 
